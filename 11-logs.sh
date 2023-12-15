92c8f595479f2bbf46981228118160e75ada7f07
Tulasi Karnati <karnatitulasi2099@gmail.com>
2023-12-15 01:34:24 +0000

    mysql scripts

--- 11-logs.sh
+++ 11-logs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 echo "script name: $0"
 VALIDATE(){
@@ -21,8 +23,8 @@ else
     echo "root user"
 fi
 
-yum install mysql -y
+yum install mysql -y &>> LOGFILE
 VALIDATE $? "Installing mysql"
 
-yum install git -y
+yum install git -y &>> LOGFILE
 VALIDATE $? "Installing git"
