a9f309cddc730a7a45fd79a3743dc3bf843eb5c0
Tulasi Karnati <karnatitulasi2099@gmail.com>
2023-12-19 14:18:09 +0000

    shell-script

--- 15-delete-old-logs.sh
+++ 15-delete-old-logs.sh
@@ -12,7 +12,7 @@ then
     echo -e "$R Source directory: $SOURCE_DIR doesn't exist $N"
 fi
 
-FILE_TO_DELETE=$(find $SOURDE_DIR -type f -mtime +14 -name "*.log")
+FILE_TO_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log")
 
 while IFS= read -r line
 do
