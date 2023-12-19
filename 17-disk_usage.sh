51e0808d3b7837e79e8de92a8743d4b41ab77cb4
Tulasi Karnati <karnatitulasi2099@gmail.com>
2023-12-19 15:37:35 +0000

    shell-script

--- 17-disk_usage.sh
+++ 17-disk_usage.sh
@@ -8,11 +8,9 @@ while IFS= read line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print $1F}')
-
-    if[ $usage -ge $DISK_THRESHOLD ]
+    if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="HIGH DISK USAGE on $partition:$usage\n"
     fi
 done <<< DISK_USAGE
-
 echo -e "message: $message"
